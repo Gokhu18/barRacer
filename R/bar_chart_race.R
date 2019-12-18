c6c1d13b8b4f647e9a67b8005fb4f63a44702036
Jamie Lendrum <jalendrum@gmail.com>
2019-12-18 11:11:40 +0000

    complete decomposition into smaller functions

--- R/bar_chart_race.R
+++ R/bar_chart_race.R
@@ -26,20 +26,20 @@ bar_chart_race <- function(df, cat_col, val_col, time_col,
   extend <- max(df %>% dplyr::pull({{val_col}})) * 1.07
 
   # fill in missing time values
-  df <- fill_missing_times(df, cat_col, val_col, time_col) 
+  df <- fill_missing_times(df, {{cat_col}}, {{val_col}}, {{time_col}}) 
   
-  df <- add_ranks(df, val_col, time_col, max_bars)
+  df <- filter_top_ranks(df, {{val_col}}, {{time_col}}, max_bars)
   
   # interpolate between finer time values for smoother transitions
-  df <- tween_more_times(df, )
+  df <- tween_more_times(df, {{cat_col}}, {{val_col}}, {{time_col}})
 
   p <- df %>%
-    ggplot2::ggplot(ggplot2::aes(x = .rank, y = new_val, fill = {{cat_col}})) +
-    ggplot2::geom_tile(ggplot2::aes(y = new_val/2, height = new_val),
+    ggplot2::ggplot(ggplot2::aes(x = .rank, y = {{val_col}}, fill = {{cat_col}})) +
+    ggplot2::geom_tile(ggplot2::aes(y = {{val_col}}/2, height = {{val_col}}),
                        show.legend = FALSE, width = 0.9) +
     ggplot2::geom_text(ggplot2::aes(label = {{cat_col}}), hjust = "right",
                        fontface = "bold", nudge_y = -nudge, size = 6) +
-    ggplot2::geom_text(ggplot2::aes(label = scales::comma(round(new_val))), hjust = "left",
+    ggplot2::geom_text(ggplot2::aes(label = scales::comma(round({{val_col}}))), hjust = "left",
                        nudge_y = nudge, colour = "grey30", size = 5) +
     ggplot2::scale_y_continuous("", labels = scales::comma, limits = c(-shift, extend)) +
     ggplot2::scale_x_reverse("") +
@@ -51,7 +51,7 @@ bar_chart_race <- function(df, cat_col, val_col, time_col,
                     text = ggplot2::element_text(size = 20),
                     plot.title = ggplot2::element_text(size = 32, face = "bold"),
                     plot.subtitle = ggplot2::element_text(size = 24)) +
-    gganimate::transition_states(new_time, transition_length = 1, state_length = 0) +
+    gganimate::transition_states({{time_col}}, transition_length = 1, state_length = 0) +
     gganimate::ease_aes("linear") +
     gganimate::enter_fly(x_loc = -(max_bars + 4)) +
     gganimate::exit_fly(x_loc = -(max_bars + 4)) #+
@@ -66,56 +66,59 @@ bar_chart_race <- function(df, cat_col, val_col, time_col,
 
 #' Title
 #'
-#' @param df 
+#' @param df A dataframe containing the data
 #' @param cat_col 
 #' @param val_col 
 #' @param time_col 
 #'
 #' @return
 #' @export
-#'
-#' @examples
 fill_missing_times <- function(df, cat_col, val_col, time_col) {
   
   df %>%
     dplyr::group_by({{cat_col}}) %>%
-    # this is a workaround as {{}} gives errors in full_seq below
-    dplyr::mutate(new_time = {{time_col}}) %>% 
-    tidyr::complete(new_time = tidyr::full_seq(new_time, 1)) %>%
-    dplyr::mutate(new_val = stats::spline(x = new_time, y = {{val_col}}, xout = new_time)$y) %>% 
-    ungroup()
+    tidyr::complete({{time_col}} := tidyr::full_seq({{time_col}}, 1)) %>%
+    dplyr::mutate({{val_col}} := stats::spline(x = {{time_col}}, y = {{val_col}}, xout = {{time_col}})$y) %>% 
+    dplyr::ungroup()
   
 }
 
 #' Title
 #'
-#' @param df 
+#' @param df A dataframe containing the data
 #' @param val_col 
 #' @param time_col 
 #' @param max_bars 
 #'
 #' @return
 #' @export
-#'
-#' @examples
-add_ranks <- function(df, val_col, time_col, max_bars) {
-  
+filter_top_ranks <- function(df, val_col, time_col, max_bars) {
+
   df %>% 
-    dplyr::group_by(new_time) %>%
-    dplyr::mutate(.rank = dplyr::min_rank(-new_val)*1) %>%
+    dplyr::group_by({{time_col}}) %>%
+    dplyr::mutate(.rank = dplyr::min_rank(-{{val_col}})*1) %>%
     dplyr::filter(.rank <= max_bars) %>%
     dplyr::ungroup()
   
 }
 
+#' Title
+#'
+#' @param df A dataframe containing the data
+#' @param cat_col 
+#' @param val_col 
+#' @param time_col 
+#'
+#' @return
+#' @export
 tween_more_times <- function(df, cat_col, val_col, time_col) {
   
   df %>% 
     dplyr::group_by({{cat_col}}) %>%
-    tidyr::complete(new_time = tidyr::full_seq(new_time, 0.5)) %>%
-    dplyr::mutate(new_val = stats::spline(x = new_time, y = new_val, xout = new_time)$y) %>%
+    tidyr::complete({{time_col}} := tidyr::full_seq({{time_col}}, 0.5)) %>%
+    dplyr::mutate({{val_col}} := stats::spline(x = {{time_col}}, y = {{val_col}}, xout = {{time_col}})$y) %>%
     # "approx" below for linear interpolation. "spline" has a bouncy effect.
-    dplyr::mutate(.rank = stats::approx(x = new_time, y = .rank, xout = new_time)$y) %>%
+    dplyr::mutate(.rank = stats::approx(x = {{time_col}}, y = .rank, xout = {{time_col}})$y) %>%
     dplyr::ungroup()
   
   
